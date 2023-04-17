7252f00e4c0b7e771b17c3d332a724e7525ebe41
Luan Glaab <129619687+luangl@users.noreply.github.com>
2023-04-17 20:51:42 -0300

    Upload Arquivos

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(a: number, b: number): number {
+  return a * b;
+}
+
+function saudar(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+console.log(multiplicar(2, 3)); 
+console.log(saudar("Luan")); 
